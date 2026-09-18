80efb796a7fcdde3f30e5aaff591549e083a4942
Gogo <anndigogo@gmail.com>
2026-09-18 17:53:42 +0200

    Added new components - 18.09.26
    Added the following components: Header, Footer, Hero, Search Engine. Additionally added a form in the search engine component template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { SearchEngineComponent } from './search-enginge/search-engine.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, HeroComponent, SearchEngineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
+
+export class AppComponent {
   protected readonly title = signal('booking-app');
 }
---
+++ src/app/search-enginge/search-engine.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-engine',
+  imports: [],
+  templateUrl: './search-engine.component.html',
+  styleUrl: './search-engine.component.css',
+})
+export class SearchEngineComponent {}
